fc4653966d64feb43f9c6f68fedfb4c175608128
melvingb <contacto@melvingarcia.com>
2015-07-07 12:49:58 -0600

    Uptade files and removed unecesary lines

---
+++ template/nav_mobile.js
@@ -0,0 +1,16 @@
+    $(function() {
+
+        var btn_movil = $('#nav-mobile'),
+            menu = $('#menu-mobile').find('ul');
+
+        // Al dar click agregar/quitar clases que permiten el despliegue del menú
+        btn_movil.on('click', function (e) {
+            e.preventDefault();
+
+            var el = $(this);
+
+            el.toggleClass('nav-active');
+            menu.toggleClass('open-menu');
+        })
+
+    });
